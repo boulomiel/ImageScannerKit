5e4c815bb07c62ba5c7eb65898e8c89c6db42ead
Ruben <55102143+boulomiel@users.noreply.github.com>
2025-03-17 07:59:39 +0200

    Added documentDetector

---
+++ ImageScannerKit/ScannerPipeline/DocumentDetector.h
@@ -0,0 +1,25 @@
+//
+//  DocumentDetector.h
+//  ImageScannerKit
+//
+//  Created by Ruben Mimoun on 17/03/2025.
+//
+
+#ifndef DocumentDetector_h
+#define DocumentDetector_h
+
+#import <UIKit/UIKit.h>
+#import <UIKit/UIKit.h>
+
+@interface DocumentDetector : NSObject
+
+ // Detects a document in the given UIImage and returns the processed image along with detected corner points.
+ // - Parameter image: The input UIImage containing the document.
+ // - Parameter completion: A callback that returns a processed UIImage and an NSArray of NSValue containing CGPoint coordinates of detected corners.
+- (void)detectDocumentIn:(UIImage *)image
+              completion:(void (^)(UIImage *processedImage, NSArray<NSValue *> *cornerPoints))completion;
+
+@end
+
+
+#endif /* DocumentDetector_h */
---
+++ ImageScannerKit/ScannerPipeline/DocumentDetector.mm
@@ -0,0 +1,63 @@
+//
+//  DocumentDetector.m
+//  ImageScannerKit
+//
+//  Created by Ruben Mimoun on 17/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+#import "DocumentDetector.h"
+#import "CameraHandler.hpp"
+#import "Erosion.hpp"
+#import "Thresholded.hpp"
+#import "GrayScale.hpp"
+#import "Blurry.hpp"
+#import "Cannyied.hpp"
+#import "ScannerStep.hpp"
+#import "DetectPolygon.hpp"
+#import "AutoSnapHandler.hpp"
+#import "PointToCGConverter.hpp"
+#import "Clahe.hpp"
+
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgcodecs/ios.h>
+
+using namespace cv;
+
+@implementation DocumentDetector
+
+- (void)detectDocumentIn:(UIImage *)image completion:(void (^)(UIImage *, NSArray<NSValue *> *))completion {
+    Mat mat, baseImage, image_copy;
+    UIImageToMat(image, mat);
+    cvtColor(mat, mat, COLOR_BGR2BGRA);
+    
+    cvtColor(mat, baseImage, COLOR_BGR2RGB);
+    mat.copyTo(image_copy);
+
+    GrayScale *grayScale = new GrayScale;
+    Blurry *blurry = new Blurry;
+    Clahe *clahe = new Clahe;
+
+    Thresholded *thresHolded = new Thresholded;
+    Erosion *openErosion = new Erosion(MORPH_OPEN);
+    Erosion *closeErosion = new Erosion(MORPH_CLOSE);
+    Cannyied *canny = new Cannyied;
+    __weak typeof(self) weakSelf = self;
+    DetectPolygon *detectPolygon = new DetectPolygon([&, weakSelf](const std::vector<cv::Point> points){
+        dispatch_async(dispatch_get_main_queue(), ^{
+            PointToCGConverter *converter = [[PointToCGConverter alloc] init];
+            completion(image, [converter convertPoint:points]);
+        });
+    });
+   
+    grayScale->setNext(blurry);
+    blurry->setNext(closeErosion);
+    closeErosion->setNext(thresHolded);
+    thresHolded->setNext(openErosion);
+    openErosion->setNext(canny);
+    canny->setNext(detectPolygon);
+    
+    grayScale->handle(image_copy);
+}
+
+@end
