a39a63521b6a5ee159d3000090fd702d54313d29
Ruben <55102143+boulomiel@users.noreply.github.com>
2025-03-27 07:20:51 +0200

    ContentView - Template
     - Implemlement onSnap
    - implement documentdetectorIos

--- ImageScannerKitTemplate/ContentView.swift
+++ ImageScannerKitTemplate/ContentView.swift
@@ -14,21 +14,81 @@ struct ContentView: View {
     
     @State var flashToggle: Bool = false
     @Environment(\.cameraHandler) var handler
+    @Environment(\.cameraEvent) var event
         
+    let detector = DocumentDetectorIOS()
+    
+    @State var image: UIImage?
+    @State var detectedPts: [CGPoint] = []
+
+
+
     var body: some View {
-        DocumentScannerView()
-            .addActionOnDocumentSnapped { pts, img in
-                print("Action Added")
-            }
-            .overlay(alignment: .top) {
-                Button("Flash") {
-                    flashToggle.toggle()
+        NavigationStack {
+            DocumentScannerView()
+                .addActionOnDocumentSnapped { pts, img in
+                    print("Action Added")
                 }
-            }
-            .onChange(of: flashToggle) { oldValue, newValue in
-                handler.setFlashEnabled(newValue)
-            }
+                .onAppear(perform: {
+                    handler.setAutoDetectionEnabled(false)
+                })
+                .overlay(alignment: .top) {
+                    Button("Flash") {
+                        flashToggle.toggle()
+                    }
+                }
+                .onChange(of: flashToggle) { oldValue, newValue in
+                    handler.setFlashEnabled(newValue)
+                }
+                .overlay(alignment: .bottom) {
+                    Button("Snap") {
+                        if let image = handler.snap() {
+                            self.image = image
+                            handler.stopCamera()
+
+                        }
+                    }
+                }
+                .navigationDestination(item: $image) { img in
+                    Image(uiImage: img)
+                        .resizable()
+                        .scaledToFit()
+                        .onAppear {
+                            detector.detectDocument(in: img) { uiImage, pts in
+                                let points = pts?.compactMap { $0.cgPointValue } ?? []
+                                self.detectedPts = points
+                            }
+                        }
+                        .overlay {
+                            if !detectedPts.isEmpty {
+                                docPath(points: detectedPts, imageSize: img.size)
+                            }
+                        }
+                }
+        }
     }
+    
+    @ViewBuilder
+    func docPath(points: [CGPoint], imageSize: CGSize) -> some View {
+        GeometryReader { geo in
+            let size = geo.size
+            let widthRatio = size.width / imageSize.width
+            let heightRatio = size.height  / imageSize.height
+            
+            let pts = points.map { CGPoint(x: $0.x * widthRatio, y: $0.y * heightRatio) }
+            
+            Path { p in
+                p.move(to: pts[0])
+                p.addLine(to: pts[1])
+                p.addLine(to: pts[2])
+                p.addLine(to: pts[3])
+                p.closeSubpath()
+            }
+            .stroke(lineWidth: 3)
+            .shadow(radius: 4)
+        }
+    }
+    
 }
 
 
